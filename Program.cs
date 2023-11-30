7dce52aaad86ead5e6b3b71e6d126e5a8edc8ecb
Jasmin Peters <peters.jasmin@gmx.at>
2023-11-30 14:24:02 +0100

    Merge to main

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Shalom!");
 Console.WriteLine("Zewas!");
-Console.WriteLine("asdf!");
-Console.WriteLine("Yeah buddy");
+Console.WriteLine("asdf");
+Console.WriteLine("Yeah buddy!");
+
