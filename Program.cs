1616281245141846dce32c427e515c4d3e95b4d5
Jasmin Peters <peters.jasmin@gmx.at>
2023-11-30 16:28:04 +0100

    Merge new

--- Program.cs
+++ Program.cs
@@ -2,4 +2,4 @@
 Console.WriteLine("Shalom!");
 Console.WriteLine("Zewas!");
 Console.WriteLine("asdf");
-Console.WriteLine("Yeah buddy changes and stuff!");
+Console.WriteLine("Yeah buddy changes and stuff!!");
