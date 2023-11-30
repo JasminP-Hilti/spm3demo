7f65293636c04d6f166cc7867ca859c00ddd6e42
Jasmin Peters <peters.jasmin@gmx.at>
2023-11-30 14:16:04 +0100

    New2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Shalom!");
 Console.WriteLine("Zewas!");
-Console.WriteLine("asdf");
+Console.WriteLine("asdf!");
 Console.WriteLine("Yeah buddy");
