f289fa448ea165f3f373dfe829035688f6249e51
Jasmin Peters <peters.jasmin@gmx.at>
2023-11-30 16:28:21 +0100

    add 3!

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Shalom!");
+Console.WriteLine("Shalom!!");
 Console.WriteLine("Zewas!");
 Console.WriteLine("asdf");
-Console.WriteLine("Yeah buddy changes and stuff!!");
+<<<<<<< HEAD
+Console.WriteLine("Yeah buddy changes and stuff!!!!");
